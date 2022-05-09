0b3d2237eeea005c5e273ab4ae0ff8cf1f6768d6
Rodrigo Luger <rodluger@gmail.com>
2022-05-09 15:31:40 -0400

    new way to handle PRs

--- src/publish.js
+++ src/publish.js
@@ -1,19 +1,37 @@
 // Imports
 const core = require("@actions/core");
-const artifact = require("@actions/artifact");
 const shell = require("shelljs");
+const github = require('@actions/github');
 
 // Exports
 module.exports = { publishOutput };
 
 // Get repo info
-const GITHUB_SLUG = shell.env["GITHUB_REPOSITORY"];
-const GITHUB_BRANCH = shell
-  .exec("echo ${GITHUB_REF##*/}", {silent: true})
-  .replace(/(\r\n|\n|\r)/gm, "");
+var GITHUB_SLUG;
+var GITHUB_BRANCH;
+var TARGET_BRANCH;
 const GITHUB_TOKEN = core.getInput("github-token");
 const GITHUB_WORKSPACE = shell.env["GITHUB_WORKSPACE"];
-const PULL_REQUEST = shell.env["GITHUB_BASE_REF"].length > 0;
+const OUTPUT_BRANCH_SUFFIX = core.getInput("output-branch-suffix");
+if (github.event_name == 'pull_request') {
+
+  // This is a pull request, so we'll force-push the output
+  // to `pull-request-<number>-pdf` on the author's repo
+  GITHUB_SLUG = github.event.pull_request.head.repo.full_name;
+  TARGET_BRANCH = `pull-request-${github.event.pull_request.number}-pdf`;
+
+} else {
+
+  // Not a pull request, so we'll force-push the output
+  // to `<branch_name>-pdf` on the same repo
+  GITHUB_SLUG = github.repository;
+  GITHUB_BRANCH = shell
+    .exec("echo ${GITHUB_REF##*/}", {silent: true})
+    .replace(/(\r\n|\n|\r)/gm, "");
+  TARGET_BRANCH = `${GITHUB_BRANCH}-${OUTPUT_BRANCH_SUFFIX}`;
+
+}
+
 
 /**
  * Publish the article output.
@@ -30,44 +48,30 @@ async function publishOutput() {
     output.push("arxiv.tar.gz");
   }
 
-  if (PULL_REQUEST) {
-    // Upload an artifact
-    const artifactClient = artifact.create();
-    const uploadResponse = await artifactClient.uploadArtifact(
-      "showyourwork-output", 
-      output, 
-      ".", 
-      {
-        continueOnError: false
-      }
-    );
-  } else {
-    // Force-push output to a separate branch
-    if ((core.getInput("output-branch-suffix").length > 0) && (GITHUB_BRANCH.length > 0)) {
-      core.startGroup("Uploading output");
-      const suffix = core.getInput("output-branch-suffix");
-      const TARGET_BRANCH = `${GITHUB_BRANCH}-${suffix}`;
-      const TARGET_DIRECTORY = shell
-        .exec("mktemp -d")
-        .replace(/(\r\n|\n|\r)/gm, "");
-      shell.cp("-R", ".", `${TARGET_DIRECTORY}`);
-      shell.cd(`${TARGET_DIRECTORY}`);
-      shell.exec(`git checkout --orphan ${TARGET_BRANCH}`);
-      shell.exec("git rm --cached -rf .", {silent: true});
-      for (const out of output) {
-        shell.exec(`git add -f ${out}`);
-      }
-      shell.exec(
-        "git -c user.name='showyourwork' -c user.email='showyourwork' " +
-          "commit -m 'force-push article output'"
-      );
-      shell.exec(
-        "git push --force " +
-          `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_SLUG} ` +
-          `${TARGET_BRANCH}`
-      );
-      shell.cd(GITHUB_WORKSPACE);
-      core.endGroup();
-    }
+  // Force-push output to a separate branch
+  core.startGroup("Uploading output");
+  const TARGET_DIRECTORY = shell
+    .exec("mktemp -d")
+    .replace(/(\r\n|\n|\r)/gm, "");
+  shell.cp("-R", ".", `${TARGET_DIRECTORY}`);
+  shell.cd(`${TARGET_DIRECTORY}`);
+  shell.exec(`git checkout --orphan ${TARGET_BRANCH}`);
+  shell.exec("git rm --cached -rf .", {silent: true});
+  for (const out of output) {
+    shell.exec(`git add -f ${out}`);
   }
+  shell.exec(
+    "git -c user.name='showyourwork' -c user.email='showyourwork' " +
+      "commit -m 'force-push article output'"
+  );
+  shell.exec(
+    "git push --force " +
+      `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_SLUG} ` +
+      `${TARGET_BRANCH}`
+  );
+  shell.cd(GITHUB_WORKSPACE);
+  core.endGroup();
+
+  // Set an action output containing the link to the PDF
+  core.setOutput("pdf-url", `https://github.com/${GITHUB_SLUG}/raw/${TARGET_BRANCH}/${config["ms_pdf"]}`);
 }
