4cd621ff43256a39ebc83ced3016c1469cb4e081
Rodrigo Luger <rodluger@gmail.com>
2022-05-10 13:52:35 -0400

    debug

--- src/publish.js
+++ src/publish.js
@@ -33,36 +33,38 @@ async function publishOutput() {
     }
   );
 
-  // Force-push output to a separate branch (if not a PR)
-  if (shell.env["GITHUB_EVENT_NAME"] != 'pull_request') {
-    core.startGroup("Uploading output");
-    const GITHUB_TOKEN = core.getInput("github-token");
-    const GITHUB_SLUG = shell.env["GITHUB_REPOSITORY"];
-    const GITHUB_WORKSPACE = shell.env["GITHUB_WORKSPACE"];
-    const GITHUB_REF = shell.env["GITHUB_REF"];
-    const OUTPUT_BRANCH_SUFFIX = core.getInput("output-branch-suffix");
-    const GITHUB_BRANCH = GITHUB_REF.split("/")[2];
-    const TARGET_BRANCH = `${GITHUB_BRANCH}-${OUTPUT_BRANCH_SUFFIX}`;
-    const TARGET_DIRECTORY = shell
-      .exec("mktemp -d")
-      .replace(/(\r\n|\n|\r)/gm, "");
-    shell.cp("-R", ".", `${TARGET_DIRECTORY}`);
-    shell.cd(`${TARGET_DIRECTORY}`);
-    shell.exec(`git checkout --orphan ${TARGET_BRANCH}`);
-    shell.exec("git rm --cached -rf .", {silent: true});
-    for (const out of output) {
-      shell.exec(`git add -f ${out}`);
-    }
-    shell.exec(
-      "git -c user.name='showyourwork' -c user.email='showyourwork' " +
-        "commit -m 'force-push article output'"
-    );
-    shell.exec(
-      "git push --force " +
-        `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_SLUG} ` +
-        `${TARGET_BRANCH}`
-    );
-    shell.cd(GITHUB_WORKSPACE);
-    core.endGroup();
+  // Force-push output to a separate branch
+  core.startGroup("Uploading output");
+  const GITHUB_SLUG = shell.env["GITHUB_REPOSITORY"];
+  const GITHUB_WORKSPACE = shell.env["GITHUB_WORKSPACE"];
+  const GITHUB_REF = shell.env["GITHUB_REF"];
+  const GITHUB_EVENT_NAME = shell.env["GITHUB_EVENT_NAME"];
+  const GITHUB_TOKEN = core.getInput("github-token");
+  const OUTPUT_BRANCH_SUFFIX = core.getInput("output-branch-suffix");
+  const GITHUB_BRANCH_OR_PR_NUMBER = GITHUB_REF.split("/")[2];
+  const TARGET_DIRECTORY = shell
+    .exec("mktemp -d")
+    .replace(/(\r\n|\n|\r)/gm, "");
+  const TARGET_BRANCH = 
+    (GITHUB_EVENT_NAME == 'pull_request') ? 
+    `pull-request-${GITHUB_BRANCH_OR_PR_NUMBER}-${OUTPUT_BRANCH_SUFFIX}` : 
+    `${GITHUB_BRANCH_OR_PR_NUMBER}-${OUTPUT_BRANCH_SUFFIX}`;
+  shell.cp("-R", ".", `${TARGET_DIRECTORY}`);
+  shell.cd(`${TARGET_DIRECTORY}`);
+  shell.exec(`git checkout --orphan ${TARGET_BRANCH}`);
+  shell.exec("git rm --cached -rf .", {silent: true});
+  for (const out of output) {
+    shell.exec(`git add -f ${out}`);
   }
+  shell.exec(
+    "git -c user.name='showyourwork' -c user.email='showyourwork' " +
+      "commit -m 'force-push article output'"
+  );
+  shell.exec(
+    "git push --force " +
+      `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_SLUG} ` +
+      `${TARGET_BRANCH}`
+  );
+  shell.cd(GITHUB_WORKSPACE);
+  core.endGroup();
 }
