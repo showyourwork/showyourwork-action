494a9d272e7d3e847a3f975968b126e78b7f77ba
Rodrigo Luger <rodluger@gmail.com>
2022-05-13 10:02:05 -0400

    add build

---
+++ src/build.js
@@ -0,0 +1,46 @@
+// Imports
+const core = require("@actions/core");
+const { setupConda } = require("./conda");
+const { buildArticle } = require("./article");
+const { buildTarball } = require("./arxiv");
+const { publishOutput } = require("./publish");
+const { publishLogs } = require("./logs");
+
+// Exports
+module.exports = { build };
+
+/**
+ * Build the article.
+ *
+ */
+async function build() {
+  try {
+    // Setup conda or restore from cache
+    await setupConda();
+
+    // Build the article
+    await buildArticle();
+
+    // Build arxiv tarball
+    if (core.getInput("build-tarball") == "true") {
+      await buildTarball();
+    }
+
+    // Publish the article output
+    await publishOutput();
+
+    // Publish the logs
+    await publishLogs();
+  } catch (error) {
+    // Publish the logs
+    try {
+      await publishLogs();
+    } catch (error) {
+      core.error("Unable to upload the build logs.");
+      core.setFailed(error.message);
+    }
+
+    // Exit gracefully
+    core.setFailed(error.message);
+  }
+}
