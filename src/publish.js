7966aac3a75bed4b332a0cbf1dabb2451816f8fc
MilesCranmer <miles.cranmer@gmail.com>
2022-07-12 10:51:39 -0400

    Execute latexdiff without perl call

--- src/publish.js
+++ src/publish.js
@@ -49,7 +49,7 @@ async function publishOutput() {
         shell.exec(`./latexpand src/tex/${config["ms_name"]} -o .flat_old.tex`);
 
         // Compute diff, and build
-        shell.exec(`perl latexdiff src/tex/.flat_old.tex src/tex/.flat_new.tex > tmp.tex`);
+        shell.exec(`./latexdiff src/tex/.flat_old.tex src/tex/.flat_new.tex > tmp.tex`);
         shell.exec(`mv tmp.tex src/tex/ms.tex`);
         shell.exec(`showyourwork build`);
         shell.exec(`cp ${config["ms_pdf"]} diff.pdf`);
