5fd00ab04453f10a7fadf08ff120681a74404e5f
Rodrigo Luger <rodluger@gmail.com>
2022-07-18 14:46:14 -0400

    Update publish.js

--- src/publish.js
+++ src/publish.js
@@ -61,7 +61,7 @@ async function publishOutput() {
         // Patch for aastex to force exiting vertical mode before trying to render strikeout text.
         // If the first character of any paragraph or section is deleted, the diff will _not_
         // compile because the strikeout (\sout) command fails in vertical mode.
-        shell.exec(`sed -i 's/\\\\RequirePackage\\[normalem\\]{ulem}/\\\\RequirePackage\\[normalem\\]{ulem} \\\\let\\\\oldsout\\\\sout \\\\renewcommand\\\\sout\\[1\\]{\\\\leavevmode\\\\oldsout{#1}}/' tmp.tex`);
+        shell.exec(`sed -i 's/\\\\RequirePackage\\[normalem\\]{ulem}/\\\\RequirePackage\\[normalem\\]{ulem} \\\\let\\\\oldsout\\\\sout \\\\renewcommand\\\\sout\\[1\\]{x\\\\oldsout{#1}}/' tmp.tex`);
         
         shell.exec(`mv tmp.tex src/tex/${config["ms_name"]}.tex`);
         shell.exec(`showyourwork build`);
